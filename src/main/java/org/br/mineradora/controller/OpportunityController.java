7fee07cf4597cce1b689a081e6e0f209cba73d88
Alison Silva <alison.dev.silva@gmail.com>
2024-10-10 00:57:19 -0300

    Add generate report route

--- src/main/java/org/br/mineradora/controller/OpportunityController.java
+++ src/main/java/org/br/mineradora/controller/OpportunityController.java
@@ -4,22 +4,42 @@ import io.quarkus.security.Authenticated;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.ServerErrorException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.br.mineradora.dto.OpportunityDTO;
 import org.br.mineradora.service.OpportunityService;
 
+import java.util.Date;
 import java.util.List;
 
 @Path("/api/opportunity")
 @Authenticated
+@Slf4j
 public class OpportunityController {
 
     @Inject
     OpportunityService opportunityService;
 
     @GET
-    @Path("/data")
-    public List<OpportunityDTO> generateReport(){
+    public List<OpportunityDTO> list(){
         return opportunityService.generateOpportunityData();
     }
 
+    @GET
+    @Path("/report")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response generateReport(){
+        try{
+            return Response.ok(opportunityService.generateCSVOpportunityReport(),
+                    MediaType.APPLICATION_OCTET_STREAM)
+                    .header("content-disposition", "attachment; filename="+new Date()+"--selling-oppotunity.csv")
+                    .build();
+        } catch (ServerErrorException e) {
+            log.error("Server error", e);
+            return Response.serverError().build();
+        }
+    }
 }
