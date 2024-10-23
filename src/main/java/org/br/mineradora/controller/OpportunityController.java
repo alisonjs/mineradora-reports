7fc6d196f16e8f18e3b5b8df3871c303d0a3d348
Alison Silva <alison.dev.silva@gmail.com>
2024-10-23 00:53:08 -0300

    Fix configurations

--- src/main/java/org/br/mineradora/controller/OpportunityController.java
+++ src/main/java/org/br/mineradora/controller/OpportunityController.java
@@ -1,18 +1,15 @@
 package org.br.mineradora.controller;
 
 import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.ServerErrorException;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.br.mineradora.dto.OpportunityDTO;
 import org.br.mineradora.service.OpportunityService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
-import java.util.Date;
 import java.util.List;
 
 @Path("/api/opportunity")
@@ -23,23 +20,13 @@ public class OpportunityController {
     @Inject
     OpportunityService opportunityService;
 
+    @Inject
+    JsonWebToken jsonWebToken;
+
     @GET
+    @RolesAllowed({"user", "manager"})
     public List<OpportunityDTO> list(){
         return opportunityService.generateOpportunityData();
     }
-
-    @GET
-    @Path("/report")
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response generateReport(){
-        try{
-            return Response.ok(opportunityService.generateCSVOpportunityReport(),
-                    MediaType.APPLICATION_OCTET_STREAM)
-                    .header("content-disposition", "attachment; filename="+new Date()+"--selling-oppotunity.csv")
-                    .build();
-        } catch (ServerErrorException e) {
-            log.error("Server error", e);
-            return Response.serverError().build();
-        }
-    }
 }
+
